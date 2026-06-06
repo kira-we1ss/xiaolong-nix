1490e82b0a4bf6660b6598b5d6d9a9e81e75711e
Kira Weiss <kweiss@kweiss.cat>
2026-06-06 08:13:21 +0300

    Add .desktop for Waywallen [skip ci]

--- pkgs/waywallen.nix
+++ pkgs/waywallen.nix
@@ -2,14 +2,22 @@
 
 let
   version = "0.1.8";
+  pname = "waywallen";
   src = fetchurl {
-    url = "https://github.com/waywallen/waywallen/releases/download/v${version}/waywallen-${version}-x86_64.AppImage";
+    url = "https://github.com/waywallen/waywallen/releases/download/v${version}/${pname}-${version}-x86_64.AppImage";
     hash = "sha256-OLEJyXTDyWzyyscU8GpwVRlEzhM3zI52ZZL53Unfoew=";
   };
+  extracted = appimageTools.extractType2 { inherit pname version src; };
 in
 appimageTools.wrapType2 {
-  inherit version src;
-  pname = "waywallen";
+  inherit pname version src;
+
+  extraInstallCommands = ''
+    install -Dm644 ${extracted}/org.waywallen.waywallen.desktop \
+      $out/share/applications/org.waywallen.waywallen.desktop
+    install -Dm644 ${extracted}/usr/share/icons/hicolor/scalable/apps/org.waywallen.waywallen.svg \
+      $out/share/icons/hicolor/scalable/apps/org.waywallen.waywallen.svg
+  '';
 
   meta = {
     description = "Dynamic wallpaper manager for Linux (Wayland)";
