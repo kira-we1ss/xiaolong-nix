7b551d6b214b3539202aed5d824c72179f654368
Kira Weiss <kweiss@kweiss.cat>
2026-07-09 17:30:12 +0300

    Disable mako temporarily [skip ci]

--- home.nix
+++ home.nix
@@ -45,7 +45,7 @@
   programs.fuzzel.enable = true; # Super+D in the default setting (app launcher)
   programs.swaylock.enable = true; # Super+Alt+L in the default setting (screen locker)
   programs.waybar.enable = true; # launch on startup in the default setting (bar)
-  services.mako.enable = true; # notification daemon
+  services.mako.enable = false; # notification daemon
   services.swayidle.enable = true; # idle management daemon
   services.polkit-gnome.enable = true; # polkit
 }
