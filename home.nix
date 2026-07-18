13def5a4698317d64efce567da787a15c6b7a70d
Kira Weiss <kweiss@kweiss.cat>
2026-07-18 18:44:31 +0300

    Re-enable Niri components

--- home.nix
+++ home.nix
@@ -11,6 +11,8 @@
     qbittorrent
     davinci-resolve
     swaybg
+    rofi-wayland
+    ghostty-bin
   ];
 
   # Fish aliases
@@ -41,11 +43,9 @@
   '';
 
   programs.home-manager.enable = true;
-  # programs.alacritty.enable = true; # Super+T in the default setting (terminal)
-  # programs.fuzzel.enable = true; # Super+D in the default setting (app launcher)
-  # programs.swaylock.enable = true; # Super+Alt+L in the default setting (screen locker)
+  programs.swaylock.enable = true; # Super+Alt+L in the default setting (screen locker)
   # programs.waybar.enable = true; # launch on startup in the default setting (bar)
-  # services.mako.enable = false; # notification daemon
-  # services.swayidle.enable = true; # idle management daemon
-  # services.polkit-gnome.enable = true; # polkit
+  services.mako.enable = true; # notification daemon
+  services.swayidle.enable = true; # idle management daemon
+  services.polkit-gnome.enable = true; # polkit
 }
